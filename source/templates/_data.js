49dc12040a9fc4827912b66fbd0dde57aa129c8c
Keks <keks@htmlacademy.ru>
2022-04-05 12:09:49 +0300

    🦊 Добавляет автоматизацию

---
+++ source/templates/_data.js
@@ -0,0 +1,16 @@
+export default {
+  "pages": [
+    {
+      "path": "index.html",
+      "name": "Главная"
+    },
+    {
+      "path": "catalog.html",
+      "name": "Каталог продукции"
+    },
+    {
+      "path": "form.html",
+      "name": "Подбор программы"
+    }
+  ]
+}
