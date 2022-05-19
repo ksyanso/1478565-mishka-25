0e5410f211d7360e596708a09875a6243eb5866d
ksyanso <sor.ksy@gmail.com>
2022-05-19 22:06:52 +0400

    Добавляет декоративные элементы на главной странице

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -12,5 +12,20 @@ export default {
       "path": "form.html",
       "name": "Вязание на заказ"
     }
-  ]
+  ],
+  "viewports": {
+    "desktop": 1150,
+    "tablet": 768,
+    "mobile": null
+  },
+  "images": {
+    "product-hit": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 315,
+      "maxdppx": 2,
+      "alt": "Три вязаные корзинки разных размеров.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    }
+  }
 }
