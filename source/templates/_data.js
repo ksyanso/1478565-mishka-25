152c05beb6b69fd9ff607693b97f3e93f21f2681
ksyanso <sor.ksy@gmail.com>
2022-05-25 21:02:05 +0400

    Добавляет стилизацию каталога

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -26,6 +26,38 @@ export default {
       "maxdppx": 2,
       "alt": "Три вязаные корзинки разных размеров.",
       "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog-rabbit": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 195,
+      "maxdppx": 2,
+      "alt": "Вязаный зайчик в голубом костюме.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog-laundry-basket": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 195,
+      "maxdppx": 2,
+      "alt": "Вязаная бело-коралловая корзинка с тремя наволочками внутри.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "catalog-toy-basket": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 195,
+      "maxdppx": 2,
+      "alt": "Вязаная бело-салатовая корзинка с мягкими игрушками внутри.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    },
+    "video": {
+      "ext": "jpg",
+      "width": 260,
+      "height": 150,
+      "maxdppx": 2,
+      "alt": "Вязаная корзинка мятного-серого цвета.",
+      "viewports": ["desktop", "tablet", "mobile"]
     }
   }
 }
